bcf7238d39f6719d5e672e901eefb256c7611bd9
chengjian <1210786785@qq.com>
2021-04-20 00:28:55 +0800

    init the base function

--- src/main/java/com/cheng/alibaba/AllocationSupplyResult.java
+++ src/main/java/com/cheng/alibaba/AllocationSupplyResult.java
@@ -24,27 +24,15 @@ public class AllocationSupplyResult {
     }
 
 
-    public static void main(String[] args) {
-        List<AllocationSupplyResult> list = new ArrayList<>();
-
-        list.add( new AllocationSupplyResult(1,1));
-        list.add( new AllocationSupplyResult(2,2));
-        list.add( new AllocationSupplyResult(3,3));
-        list.add( new AllocationSupplyResult(4,4));
-        list.add( new AllocationSupplyResult(5,5));
-        list.add( new AllocationSupplyResult(6,6));
-        list.add( new AllocationSupplyResult(7,7));
-        list.add( new AllocationSupplyResult(8,8));
-        list.add( new AllocationSupplyResult(9,9));
-        list.add( new AllocationSupplyResult(10,10));
 
 
-
-
-        Allocate allocate = new Allocate();
-        List<AllocationSupplyResult> afterList = allocate.allocate(list, 15);
-
-        System.out.println(afterList.toArray());
+    @Override
+    public String toString() {
+        return "AllocationSupplyResult{" +
+                "fundPoolId=" + fundPoolId +
+                ", preAllocationShotfalls=" + preAllocationShotfalls +
+                ", allocationShotfalls=" + allocationShotfalls +
+                ", afterAllocationShotfalls=" + afterAllocationShotfalls +
+                '}';
     }
-
 }
---
+++ src/main/java/com/cheng/alibaba/BizValidation.java
@@ -0,0 +1,29 @@
+package com.cheng.alibaba;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BizValidation {
+
+    public static void main(String[] args) {
+        List<AllocationSupplyResult> list = new ArrayList<>();
+
+        list.add( new AllocationSupplyResult(1,1));
+        list.add( new AllocationSupplyResult(2,2));
+        list.add( new AllocationSupplyResult(3,3));
+        list.add( new AllocationSupplyResult(4,4));
+        list.add( new AllocationSupplyResult(5,5));
+        list.add( new AllocationSupplyResult(6,6));
+        list.add( new AllocationSupplyResult(7,7));
+        list.add( new AllocationSupplyResult(8,8));
+        list.add( new AllocationSupplyResult(9,9));
+        list.add( new AllocationSupplyResult(10,10));
+
+        Allocate allocate = new Allocate();
+        List<AllocationSupplyResult> afterList = allocate.allocate(list, 15);
+
+        afterList.stream().forEach(e -> {
+            System.out.println(e.toString());
+        });
+    }
+}
